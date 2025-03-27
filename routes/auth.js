04923cf942e87782f0af238b433a8a8fa7b34de3
Cytezera <ezrachin05@gmail.com>
2025-03-28 00:40:11 +0800

    auth.js done

--- routes/auth.js
+++ routes/auth.js
@@ -1,17 +1,23 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const jwt = require("jsonwebtoken");
+const authMiddleware = require("../midleware/authMiddleware");
+const User = require("../models/User");
+
+
 
 router.post('/login', (req,res)=>{
-	const email = req.body.username
-	const user = { name : email} 
+	const {email, password } = req.body;
+	const user = await User.find({email});
+	if (!user) return res.status(401).json({err: "Inavlid email"});
+	if (password !== user.password){
+		return res.status(401).json({err: "Wrong password" });
+	}	
 	
+		
 	const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN)		
 	res.json({ accessToken: accessToken }) 	
 })
 
-function authenticateToken(req, res, next){
-	const authHeader = req.headers['
-
-}
 
 module.export = router
