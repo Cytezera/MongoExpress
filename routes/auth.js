01f15576ca2a582241f7b949f910a9020972a887
Cytezera <ezrachin05@gmail.com>
2025-03-27 23:13:57 +0800

    Added middleware file

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,17 @@
 const express = require('express');
 const router = express.Router();
 
-router.get
+router.post('/login', (req,res)=>{
+	const email = req.body.username
+	const user = { name : email} 
+	
+	const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN)		
+	res.json({ accessToken: accessToken }) 	
+})
+
+function authenticateToken(req, res, next){
+	const authHeader = req.headers['
+
+}
 
 module.export = router
